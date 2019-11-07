a3daf9965f7896adfc75cf7f35d024964e417115
Lucas <825816425@qq.com>
2019-11-07 14:39:11 +0800

    feat(代码检查工具): 对Eslint和Stylelint检查命令进行重新配置，对commit中的钩子命令进行修改（此处需要测试）

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 // vue-cli参考教程：https://cli.vuejs.org/zh/config/
-const path = require('path'); // 引入path模块（node）
-const resolve = dir => path.join(__dirname, dir); // 将文件组成绝对路径
+const path = require('path');
+const resolve = dir => path.join(__dirname, dir);
 
 module.exports = {
     // 在htmlWebpackPlugin中增加环境变量，在index.html中使用
